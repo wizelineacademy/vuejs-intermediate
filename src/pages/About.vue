6fe8798a43287b1504297dd737044cf8d1023ebc
Eder Díaz <ederchrono@gmail.com>
2020-06-13 01:12:25 -0500

    :sparkles: add form to site

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -10,103 +10,223 @@
       <ul class="text-sm text-gray-700 list-none p-0 flex items-center">
         <li>
           <a href="#" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline"
-            >Products</a
+            >Printer</a
           >
         </li>
         <li>
           <a href="#" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline"
-            >Pricing</a
+            >Colors</a
           >
         </li>
         <li>
           <a href="#" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline"
-            >About Us</a
-          >
-        </li>
-        <li>
-          <a href="#" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline"
-            >Status</a
+            >Testimonials</a
           >
         </li>
       </ul>
       <ul class="text-sm text-gray-700 list-none p-0 flex items-center">
         <li>
-          <a href="#" class="inline-block py-2 px-3 text-gray-900 hover:text-gray-700 no-underline"
-            >Log In</a
-          >
-        </li>
-        <li>
           <button class="bg-black hover:bg-text-gray-800 text-white ml-4 py-2 px-3">
-            Sign Up
+            Buy
           </button>
         </li>
       </ul>
     </nav>
-    <section class="bg-gray-300 font-sans">
-      <div class="container m-auto flex flex-col md:flex-row max-w-xl">
-        <div class="flex flex-col w-full lg:w-1/2 justify-center items-start py-8">
-          <label for="tagline" class="uppercase tracking-loose">best app ever</label>
-          <h1 class="my-4 font-normal">Our Work Flow</h1>
-          <p class="leading-normal mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ex ea commodo
-            consequat. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-            natus error sit.
-          </p>
-          <button
-            class="bg-transparent hover:bg-black text-black font-semibold
-              hover:text-white py-2 px-4 border border-black hover:border-transparent"
-          >
-            Learn More
-          </button>
-        </div>
-        <div class="w-full lg:w-1/2 lg:py-6">
-          <img
-            src="https://stitches.hyperyolo.com/images/demo-devices.png"
-            alt="image"
-            class="w-full"
-          />
-        </div>
+    <section class="font-sans flex flex-col lg:flex-row">
+      <div class="w-full lg:w-1/2">
+        <img src="../assets/img/1DPrinter.jpg" alt="" />
+      </div>
+      <div class="w-full lg:w-1/2 flex flex-col items-center justify-center text-center p-8">
+        <h1 class="my-4 font-normal">App Features</h1>
+        <p class="leading-normal mb-4 w-2/3 text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </p>
       </div>
     </section>
+
+    <section
+      class="my-8 font-sans container max-w-xl m-auto flex flex-col
+        lg:flex-row text-center lg:text-left sm:items-center"
+    >
+      <div class="w-full lg:w-1/2">
+        <img src="../assets/img/black-toner.jpg" alt="" />
+      </div>
+      <div
+        class="w-full lg:w-1/2 flex flex-col justify-center items-start p-8
+          items-center lg:items-start"
+      >
+        <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold"
+          >best app ever</label
+        >
+        <h1 class="my-4 font-normal">Our Work Flow</h1>
+        <p class="leading-normal mb-4 text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </section>
+    <section
+      class="my-8 pt-8 font-sans container max-w-xl m-auto flex
+        flex-col-reverse lg:flex-row text-center lg:text-left sm:items-center"
+    >
+      <div
+        class="w-full lg:w-1/2 flex flex-col justify-center items-start p-8
+          items-center lg:items-start"
+      >
+        <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold"
+          >best app ever</label
+        >
+        <h1 class="my-4 font-normal">Our Work Flow</h1>
+        <p class="leading-normal mb-4 text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="w-full lg:w-1/2">
+        <img src="../assets/img/cyan-toner.jpg" alt="" />
+      </div>
+    </section>
+    <section
+      class="my-8 font-sans container max-w-xl m-auto flex flex-col
+        lg:flex-row text-center lg:text-left sm:items-center"
+    >
+      <div class="w-full lg:w-1/2">
+        <img src="../assets/img/magenta-toner.jpg" alt="" />
+      </div>
+      <div
+        class="w-full lg:w-1/2 flex flex-col justify-center items-start p-8
+          items-center lg:items-start"
+      >
+        <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold"
+          >best app ever</label
+        >
+        <h1 class="my-4 font-normal">Our Work Flow</h1>
+        <p class="leading-normal mb-4 text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </section>
+    <section
+      class="my-8 pt-8 font-sans container max-w-xl m-auto flex
+        flex-col-reverse lg:flex-row text-center lg:text-left sm:items-center"
+    >
+      <div
+        class="w-full lg:w-1/2 flex flex-col justify-center items-start p-8
+          items-center lg:items-start"
+      >
+        <label for="tagline" class="uppercase tracking-wide text-gray-700 font-bold"
+          >best app ever</label
+        >
+        <h1 class="my-4 font-normal">Our Work Flow</h1>
+        <p class="leading-normal mb-4 text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div class="w-full lg:w-1/2">
+        <img src="../assets/img/yellow-toner.jpg" alt="" />
+      </div>
+    </section>
+
     <form class="font-sans text-sm rounded w-full max-w-md mx-auto my-8 px-8 pt-6 pb-8">
       <div class="relative border rounded mb-4 shadow appearance-none label-floating">
         <input
           class="w-full py-2 px-3 text-gray-700 leading-normal rounded"
-          id="username"
-          type="text"
-          placeholder="Username"
+          id="email"
+          type="email"
+          placeholder="Email"
         />
         <label
           class="absolute block text-gray-700 top-0 left-0 w-full px-3 py-2 leading-normal"
-          for="username"
+          for="email"
         >
-          Username
+          Email
         </label>
       </div>
       <div class="relative border rounded mb-4 shadow appearance-none label-floating">
         <input
           class="w-full py-2 px-3 text-gray-700 leading-normal rounded"
-          id="password"
-          type="password"
-          placeholder="Password"
+          id="phone"
+          type="phone"
+          placeholder="Phone"
         />
         <label
           class="absolute block text-gray-700 top-0 left-0 w-full px-3 py-2 leading-normal"
-          for="password"
+          for="phone"
         >
-          Password
+          Phone
         </label>
       </div>
+      <div class="relative border rounded mb-4 shadow appearance-none label-floating">
+        <select class="w-full py-2 px-3 text-gray-700 leading-normal rounded" id="quantity">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+        <label
+          class="absolute block text-gray-700 top-0 left-0 w-full px-3 py-2 leading-normal"
+          for="quantity"
+        >
+          Quantity (max 3)
+        </label>
+      </div>
+
+      <div class="flex justify-between">
+        <div class="relative border rounded mb-4 shadow appearance-none label-floating">
+          <select class="w-full py-2 px-3 text-gray-700 leading-normal rounded" id="quantity">
+            <option value="black">Black</option>
+            <option value="cyan">Cyan</option>
+            <option value="yellow">Yellow</option>
+            <option value="magenta">Magenta</option>
+          </select>
+          <label
+            class="absolute block text-gray-700 top-0 left-0 w-full px-3 py-2 leading-normal"
+            for="quantity"
+          >
+            Color #1
+          </label>
+        </div>
+
+        <div class="relative border rounded mb-4 shadow appearance-none label-floating">
+          <select class="w-full py-2 px-3 text-gray-700 leading-normal rounded" id="quantity">
+            <option value="black">Black</option>
+            <option value="cyan">Cyan</option>
+            <option value="yellow">Yellow</option>
+            <option value="magenta">Magenta</option>
+          </select>
+          <label
+            class="absolute block text-gray-700 top-0 left-0 w-full px-3 py-2 leading-normal"
+            for="quantity"
+          >
+            Color #2
+          </label>
+        </div>
+
+        <div class="relative border rounded mb-4 shadow appearance-none label-floating">
+          <select class="w-full py-2 px-3 text-gray-700 leading-normal rounded" id="quantity">
+            <option value="black">Black</option>
+            <option value="cyan">Cyan</option>
+            <option value="yellow">Yellow</option>
+            <option value="magenta">Magenta</option>
+          </select>
+          <label
+            class="absolute block text-gray-700 top-0 left-0 w-full px-3 py-2 leading-normal"
+            for="quantity"
+          >
+            Color #3
+          </label>
+        </div>
+      </div>
+
       <div class="flex items-center justify-between">
-        <button class="bg-black hover:bg-black text-white py-2 px-4" type="button">
-          Sign In
+        <button class="w-full bg-black hover:bg-black text-white py-2 px-4" type="button">
+          Buy
         </button>
-        <a class="inline-block align-baseline text-gray-500 hover:text-gray-700" href="#">
-          Forgot Password?
-        </a>
       </div>
     </form>
+
     <section class="font-sans container m-auto text-center py-8">
       <label for="tagline" class="uppercase tracking-wide font-bold text-gray-700"
         >Various Aspects</label
